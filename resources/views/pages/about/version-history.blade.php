e8b4b09ead37f179b41dfe0fcc5bdb18cbd3c8de
luisjg <luisjguzman87@gmail.com>
2018-02-05 13:26:30 -0800

    Upgrade lumen proxypass and add force https package.

--- resources/views/pages/about/version-history.blade.php
+++ resources/views/pages/about/version-history.blade.php
@@ -10,6 +10,14 @@
 
 @section('content')
     <h1 class="type--header type--thin">Version History</h1>
+    <h2>Media 1.0.1 <small>Release Date: 02/06/18</small></h2>
+    <p>
+        <strong>Improvements:</strong>
+        <ol>
+            <li>HTTPS can now be enforced at the application level.</li>
+        </ol>
+    </p>
+    <hr>
     <h2>Media 1.0.0 <small>Release Date: 12/21/17</small></h2>
     <p>
         <strong>New Features:</strong>
