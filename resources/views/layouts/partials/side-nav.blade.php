fc04708f0f9509ff15db8b60a9f32f5c5dd02792
luisjg <luisjguzman87@gmail.com>
2018-02-06 07:46:09 -0800

    Fix the navigation links

--- resources/views/layouts/partials/side-nav.blade.php
+++ resources/views/layouts/partials/side-nav.blade.php
@@ -1,13 +1,15 @@
 {{--This section is for the future implementation of a sidebar feature--}}
-<a class="header--sm" href="{{ url('/') }}"><strong>DOCUMENTATION</strong></a>
-	<ul class="nav">
-		<li class="nav__item"><a class="nav__link" href="{{ url('/#introduction') }}">Introduction</a></li>
-		<li class="nav__item"><a class="nav__link" href="{{ url('/#howtouse') }}">How to use</a></li>
-		<li class="nav__item"><a class="nav__link" href="{{ url('/#subcollection') }}">Subcollection</a></li>
-		<li class="nav__item"><a class="nav__link" href="{{ url('/#instance') }}">Instance</a></li>
-		<li class="nav__item"><a class="nav__link" href="{{ url('/#query') }}">Query</a></li>
-		<li class="nav__item"><a class="nav__link" href="{{ url('/#examples') }}">Usage Example</a></li>
+<p class="header--sm"><strong>DOCUMENTATION</strong></p>
+<ul class="nav">
+	<li class="nav__item"><a class="nav__link" href="{{ url('/#introduction') }}">Introduction</a></li>
+	<li class="nav__item"><a class="nav__link" href="{{ url('/#howtouse') }}">How to use</a></li>
+	<li class="nav__item"><a class="nav__link" href="{{ url('/#subcollection') }}">Subcollection</a></li>
+	<li class="nav__item"><a class="nav__link" href="{{ url('/#instance') }}">Instance</a></li>
+	<li class="nav__item"><a class="nav__link" href="{{ url('/#query') }}">Query</a></li>
+	<li class="nav__item"><a class="nav__link" href="{{ url('/#examples') }}">Usage Example</a></li>
 
-	</ul>
-<a class="header--sm" href="{{ url('about/version-history') }}"><strong>Version History</strong></a>
-<br><br>
+</ul>
+<p class="header--sm"><strong>VERSION HISTORY</strong></p>
+<ul class="nav">
+	<li class="nav__item"><a class="nav__link" href="{{ url('/about/version-history') }}">Recent Changes</a></li>
+</ul>
