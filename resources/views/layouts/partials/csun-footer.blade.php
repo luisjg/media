59740c4553411070a69faf778c725eb3b9ef1390
luisjg <luisjguzman87@gmail.com>
2018-02-06 08:24:15 -0800

    Fix up the footer section.

--- resources/views/layouts/partials/csun-footer.blade.php
+++ resources/views/layouts/partials/csun-footer.blade.php
@@ -8,9 +8,9 @@
                     </div>
                     <div class="col-sm-9">
                         <ul class="list--unstyled">
-                            <li><strong>Electronic Thesis and Dissertation</strong><br />© California State University, Northridge</li>
+                            <li><strong>Media Web Service</strong><br />© California State University, Northridge</li>
                             <li>18111 Nordhoff Street, Northridge, CA 91330</li>
-                            <li>Phone: (818) 677-1200 / <a href="http://www.csun.edu/contact" target="_blank">Contact Us</a></li>
+                            <li>Phone: (818) 677-1200 / <a href="//www.csun.edu/contact" target="_blank">Contact Us</a></li>
                         </ul>
                     </div>
                 </div>
@@ -19,20 +19,20 @@
                 <div class="row">
                     <div class="col-sm-4">
                         <ul class="list--unstyled">
-                            <li><a href="http://www.csun.edu/emergency/" target="_blank">Emergency Information</a></li>
-                            <li><a href="http://www.csun.edu/afvp/university-policies-procedures/" target="_blank">University Policies &amp; Procedures</a></li>
+                            <li><a href="//www.csun.edu/emergency/" target="_blank">Emergency Information</a></li>
+                            <li><a href="//www.csun.edu/afvp/university-policies-procedures/" target="_blank">University Policies &amp; Procedures</a></li>
                         </ul>
                     </div>
                     <div class="col-sm-4">
                         <ul class="list--unstyled">
-                            <li><a href="http://www.csun.edu/sites/default/files/900-12.pdf" target="_blank">Terms and Conditions for Use</a></li>
-                            <li><a href="http://www.csun.edu/sites/default/files/500-8025.pdf" target="_blank">Privacy Policy</a></li>
-                            <li><a href="http://www.csun.edu/it/document-viewers" target="_blank">Document Reader</a></li>
+                            <li><a href="//www.csun.edu/sites/default/files/900-12.pdf" target="_blank">Terms and Conditions for Use</a></li>
+                            <li><a href="//www.csun.edu/sites/default/files/500-8025.pdf" target="_blank">Privacy Policy</a></li>
+                            <li><a href="//www.csun.edu/it/document-viewers" target="_blank">Document Reader</a></li>
                         </ul>
                     </div>
                     <div class="col-sm-4">
                         <ul class="list--unstyled">
-                            <li><a href="https://www2.calstate.edu/" target="_blank">California State University</a></li>
+                            <li><a href="//www2.calstate.edu/" target="_blank">California State University</a></li>
                         </ul>
                     </div>
                 </div>
