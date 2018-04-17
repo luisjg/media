44fd565dfd2ab6c9047852abd33f3b9ace31e0b6
luisjg <luis.guzman@metalab.csun.edu>
2018-04-17 15:32:42 -0700

    Update the version history

--- resources/views/pages/about/version-history.blade.php
+++ resources/views/pages/about/version-history.blade.php
@@ -10,7 +10,15 @@
 
 @section('content')
     <h1 class="type--header type--thin">Version History</h1>
-    <h2>Media 1.0.1 <small>Release Date: 02/06/18</small></h2>
+    <h2>{{ env('APP_NAME') }} 1.0.2 <small>Release Date: 04/09/18</small></h2>
+    <p>
+        <strong>Improvements:</strong>
+        <ol>
+            <li>Ability to upload a profile picture.</li>
+        </ol>
+    </p>
+    <hr>
+    <h2>{{ env('APP_NAME') }} 1.0.1 <small>Release Date: 02/06/18</small></h2>
     <p>
         <strong>Improvements:</strong>
         <ol>
@@ -18,7 +26,7 @@
         </ol>
     </p>
     <hr>
-    <h2>Media 1.0.0 <small>Release Date: 12/21/17</small></h2>
+    <h2>{{ env('APP_NAME') }} 1.0.0 <small>Release Date: 12/21/17</small></h2>
     <p>
         <strong>New Features:</strong>
     <ol>
