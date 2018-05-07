aa85108ba4c9cf9410b1b8828b2c0e90fd5cf549
luisjg <luis.guzman@metalab.csun.edu>
2018-05-07 13:04:36 -0700

    Add a missing side-nav section.

--- resources/views/layouts/partials/side-nav.blade.php
+++ resources/views/layouts/partials/side-nav.blade.php
@@ -2,6 +2,7 @@
 <p class="header--sm"><strong>DOCUMENTATION</strong></p>
 <ul class="nav">
 	<li class="nav__item"><a class="nav__link" href="{{ url('/#introduction') }}">Introduction</a></li>
+	<li class="nav__item"><a class="nav__link" href="{{ url('/#getting-started') }}">Getting Started</a></li>
 	<li class="nav__item"><a class="nav__link" href="{{ url('/#collections') }}">Collections</a></li>
 	<li class="nav__item"><a class="nav__link" href="{{ url('/#subcollections') }}">Subcollections</a></li>
 	<li class="nav__item"><a class="nav__link" href="{{ url('/#usage-example') }}">Usage Example</a></li>
