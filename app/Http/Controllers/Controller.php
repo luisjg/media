3eaa1bfb57372223e6adb638cdf6f33b16913cad
luisjg <luis.guzman@metalab.csun.edu>
2018-08-21 11:36:24 -0700

    Revert all the changes.

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,7 +6,6 @@ use App\Classes\ResponseHelper;
 use GuzzleHttp\Client;
 use GuzzleHttp\Promise\RejectionException;
 use Illuminate\Support\Facades\Cache;
-use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Storage;
 use Laravel\Lumen\Routing\Controller as BaseController;
 
@@ -70,11 +69,7 @@ class Controller extends BaseController
             if ($result['data'][0]['recording_link']) {
                 $nameRecording = $result['data'][0]['recording_link'];
                 Cache::add($emailUri.':audio', $nameRecording, env('APP_CACHE_DURATION'));
-                File::put(storage_path($emailUri.'.mp3'), fopen($nameRecording, 'r'));
-                $response = response()->download(storage_path($emailUri.'.mp3'));
-                return $response;
-//                return response()->file(storage_path('app/'.$emailUri.'.mp3'));
-//                return redirect($nameRecording, 301);
+                return redirect($nameRecording, 301);
             }
         }
         return ResponseHelper::customErrorMessage('Resource was not found for '.$emailUri);
@@ -149,11 +144,9 @@ class Controller extends BaseController
     {
         $fileDestination = 'media/'.$type.'/'.$emailUri.'/';
         if (Storage::exists($fileDestination.'avatar.jpg')) {
-            return Storage::get($fileDestination.'avatar.jpg');
-//            return response()->download(Storage::url($fileDestination.'avatar.jpg'));
+            return Storage::url($fileDestination.'avatar.jpg');
         } else {
-            return Storage::get('profile-default.png');
-//            return response()->download(Storage::url('media/profile-default.png'));
+            return Storage::url('profile-default.png');
         }
     }
 
@@ -168,11 +161,9 @@ class Controller extends BaseController
     {
         $fileDestination = 'media/'.$type.'/'.$emailUri.'/';
         if (Storage::exists($fileDestination.'official.jpg')) {
-            return Storage::get($fileDestination.'official.jpg');
-//            return response()->download(Storage::url($fileDestination.'official.jpg'));
+            return Storage::url($fileDestination.'official.jpg');
         } else {
-            return Storage::get('profile-default.png');
-//            return response()->download(Storage::url('media/profile-default.png'));
+            return Storage::url('profile-default.png');
         }
     }
 
@@ -185,11 +176,9 @@ class Controller extends BaseController
     {
         $fileDestination = 'media/'.$type.'/'.$emailUri.'/';
         if (Storage::exists($fileDestination.'likeness.jpg')) {
-            return Storage::get($fileDestination.'likeness.jpg');
-//            return response()->download(Storage::url($fileDestination.'likeness.jpg'));
+            return Storage::url($fileDestination.'likeness.jpg');
         } else {
-            return Storage::get('profile-default.png');
-//            return response()->download(Storage::url('media/profile-default.png'));
+            return Storage::url('profile-default.png');
         }
     }
 
