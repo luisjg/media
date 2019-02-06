6215be3d4f62955fd5519a382ab46abfeaeb7410
luisjg <luis.guzman@metalab.csun.edu>
2019-02-06 10:53:23 -0800

    Make a new post route since we're sending form parameters.

--- routes/1.1.php
+++ routes/1.1.php
@@ -1,7 +1,7 @@
 <?php
 
 $router->post('/{emailUri}/photo', 'MediaController@storeImage');
-$router->delete('/{emailUri}/photo', 'MediaController@deleteImage');
+$router->post('/{emailUri}/photo/delete', 'MediaController@deleteImage');
 
 $router->group(['prefix' => '{type}/media/{emailUri}',  'middleware' => 'cors'], function () use ($router) {
     $router->get('/', 'EntityController@getPersonsMedia');
